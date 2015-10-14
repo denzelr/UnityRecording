247f7d6583fdff9df31d89f2b61c6a97899a3486
denzelr <ryan.denzel@colorado.edu>
2015-10-14 17:54:40 -0600

    Unity scene with rotating triangles

---
+++ Video/Assets/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	void Update() {
+		transform.Rotate(Vector3.forward * Time.deltaTime*60);
+	}
+}
---
+++ Video/Assets/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Triangle : MonoBehaviour {
+	void Start() {
+		gameObject.AddComponent<MeshFilter>();;
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.Clear();
+		mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(-2, 4, 0), new Vector3(2, 4, 0)};
+		mesh.uv = new Vector2[] {new Vector2(2, 4), new Vector2(-2, 4), new Vector2(0, 0)};
+		mesh.triangles = new int[] {0, 1, 2};
+	}
+
+	void Update() {
+		transform.Rotate(Vector3.forward * Time.deltaTime*45);
+	}
+}
