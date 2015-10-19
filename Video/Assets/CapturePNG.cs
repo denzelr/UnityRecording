295e5974ae57a3ba34895710db92635107a55e98
denzelr <ryan.denzel@colorado.edu>
2015-10-19 17:58:52 -0600

    records only when toggle is checked, needs better method

--- Video/Assets/CapturePNG.cs
+++ Video/Assets/CapturePNG.cs
@@ -3,14 +3,25 @@ using System.Collections;
 
 public class CapturePNG : MonoBehaviour {
 
+	public bool record = false;
 	int counter = 0;
 
+	void FixedUpdate(){
+		if (record == true) {
+			InvokeRepeating ("Screenshot", 1, 0.04F);
+		}
+	}
+
 	void Screenshot() {
-		Application.CaptureScreenshot ("Output/" + counter + ".png");
 		counter++;
+		Application.CaptureScreenshot ("Output/" + counter + ".png");
 	}
 
 	public void invoke(){
-		InvokeRepeating ("Screenshot", 2, 0.04F);
+		if (record == false) {
+			record = true;
+		} else {
+			record = false;
+		}
 	}
 }
