d84fadf92c93220d585112383bc019e753f9b5fd
Matt Binsfeld <mbinsfeld@gmail.com>
2015-10-27 23:32:32 -0600

    Got recording working really well. The numbering scheme I am currently using supports roughly 28.9 days of video. If that's a problem I think some one died doing their exercises.

--- Video/Assets/CapturePNG.cs
+++ Video/Assets/CapturePNG.cs
@@ -4,48 +4,85 @@ using System;
 using System.IO;
 using System.Diagnostics;
 
-public class CapturePNG : MonoBehaviour {
+public class CapturePNG : MonoBehaviour
+{
 
-	private bool record = false;
-	private int counter = 0;
-	private readonly string outPath = "Output/";
-	private int vidCount = 0;
-	private readonly float frameRate = 24.0f;
-	private float recordStep;
+    private bool record = false;
+    private int counter = 0;
+    private readonly string outPath = "Output/";
+    private int vidCount = 0;
+    private readonly float frameRate = 24.0f;
+    private float recordStep;
+    private int resWidth = 750;
+    private int resHeight = 360;
+    private Camera camera;
+    void Start()
+    {
+        record = false;
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        recordStep = (1 / frameRate);
+        if (!Directory.Exists(outPath))
+        {
+            Directory.CreateDirectory(outPath);
+        }
+    }
 
-	void Start(){
-		recordStep = 1 / frameRate;
-		if (!Directory.Exists (outPath)) {
-			Directory.CreateDirectory(outPath);
-		}
-	}
+    IEnumerator Screenshot()
+    {
+        if (!Directory.Exists(outPath))
+        {
+            Directory.CreateDirectory(outPath);
+        }
+        while (record)
+        {
+            /*counter++;
+            Application.CaptureScreenshot(outPath + counter.ToString("D8") + ".png", 0);
+            UnityEngine.Debug.Log("PNG Created " + counter);
+            yield return new WaitForSeconds(recordStep);*/
+            counter++;
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            camera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            camera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            camera.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = outPath + counter.ToString("D8") + ".png";//8 didget naming scheme limits us to videos roughly 28.9 days long
+            System.IO.File.WriteAllBytes(filename, bytes);
+            UnityEngine.Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            yield return new WaitForSeconds(recordStep);
+            }
+        yield break;
+    }
 
-	IEnumerator Screenshot() {
-		while (record) {
-			counter++;
-			Application.CaptureScreenshot (outPath + counter.ToString ("D8") + ".png");
-			UnityEngine.Debug.Log ("PNG Created " + counter);
-			yield return new WaitForSeconds(recordStep);
-		}
-		MakeAvi ();
-		yield break;
-	}
-
-	public void invoke(){
-		record = !record;
-		if (record) {
-			StartCoroutine ("Screenshot");
-		}
-	}
-	private void MakeAvi(){
-		//this doesn't work
-		UnityEngine.Debug.Log ("making avi");
-		Process p = new Process();
-		p.StartInfo.UseShellExecute = false;
-		p.StartInfo.RedirectStandardOutput = true;
-		p.StartInfo.FileName = "ffmpeg -framerate 24 -i Output/%08d.png -pix_fmt yuv420p out.mp4";
-		p.Start();
-		p.WaitForExit ();
-		System.Diagnostics.Process.Start ("rm Output/*");
-	}
+    public void ToggleRecord()
+    {
+        record = !record;
+        if (record)
+        {
+            StartCoroutine("Screenshot");
+        }
+        else
+        {
+            MakeAvi();
+            Clean();
+            counter = 0;
+        }
+    }
+    private void MakeAvi()
+    {
+        string cmdTxt = "/c ffmpeg -framerate 24 -i Output/%08d.png out" + vidCount + ".mp4";
+        var process = Process.Start("C:\\Windows\\System32\\cmd.exe", cmdTxt);
+        process.WaitForExit();
+        vidCount++;
+    }
+    private void Clean()
+    {
+        string cmdTxt = "/c rmdir Output /s /q";
+        var process = Process.Start("C:\\Windows\\System32\\cmd.exe", cmdTxt);
+        process.WaitForExit();
+    }
 }
